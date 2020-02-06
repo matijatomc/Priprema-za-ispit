55eb81ebaf80aea1b10e60895bfed309946ace3d
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-02-06 11:02:29 +0100

    Add files via upload

---
+++ tomcMatijaZadatak1/tomcMatijaZadatak1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, a, b, c;
+            Console.WriteLine("Upiši a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Upiši b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Upiši c:");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            x = (a + b + c) / 3;
+
+            Console.WriteLine("Rješenje je:" + x);
+            Console.ReadKey();
+        }
+    }
+}
